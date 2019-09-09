0ed661358839adaf555637e316786683e17c8fe2
Svetlana Linuxenko <linuxenko@gmail.com>
2019-09-09 16:56:24 +0300

    Test with 595

--- lcd-shift/main.c
+++ lcd-shift/main.c
@@ -2,7 +2,7 @@
  * File Name     : main.c
  * Created By    : Svetlana Linuxenko
  * Creation Date : [2019-09-07 19:56]
- * Last Modified : [2019-09-09 16:31]
+ * Last Modified : [2019-09-09 16:56]
  * Description   :
  **********************************************************************************/
 
@@ -20,12 +20,16 @@ int main(void) {
   ShiftLCD lcd;
 
   createShift(&icS, &DDRD, &PORTD, PORTD0, PORTD1, PORTD2);
-  icS.type = IC_TYPE_HC164;
+  icS.type = IC_TYPE_HC595;
 
-  createShiftLCD(&lcd, &icS, 4, 5, 0, 1, 2, 3, 16, 2, 0);
+/*  createShiftLCD(&lcd, &icS, 4, 5, 0, 1, 2, 3, 16, 2, 0);*/
+  createShiftLCD(&lcd, &icS, 1, 3, 4, 5, 6, 7, 16, 2, 0);
   shiftLCDPuts(&lcd, "hello");
 
   while(1) {
+    shiftLCDSetCursor(&lcd, 0,0);
+    shiftLCDPuts(&lcd, "hello");
+    _delay_ms(1000);
   }
 
   return 0;
